06339f10ecc5750ba9eb58bf3ef4a36c477c08f1
Muhammad Hassan <mhassan4392@gmail.com>
2020-12-21 13:04:40 +0500

    make it better

--- public/dist/dist/client/8ca1c9d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{793:function(t,e,n){"use strict";n.r(e);var l={middleware:["auth"],auth:"guest",layout:"auth"},c=n(37),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("nuxt-child")}),[],!1,null,null,null);e.default=component.exports}}]);
